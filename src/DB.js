3b7557eb772c7fc7c3491c362d5c45efd50731e0
Aleksandr <aleksandrspopov812@gmail.com>
2019-10-23 02:36:12 -0400

    Added multi-room functionality

---
+++ src/DB.js
@@ -0,0 +1,73 @@
+var pg = require('pg')
+
+// Creating connection
+var connectionString = 'postgres://localhost/ChatDB'
+const client = new pg.Client(connectionString);
+client.connect();
+
+// Exported databased access functions
+module.exports = {
+
+	// Gets all chatroom names
+	init : function (callback) {
+
+		query = "SELECT c.name FROM chatrooms c"
+
+		client.query(query, (err, res) => {
+
+			if (err) {
+				console.log(err.stack)
+				return
+			} else {
+
+				return callback(res)
+			}
+		})
+	},
+
+	// Get all the chat history for a chatroom, sends it to a user
+	getHistory : function(socketUsername, chatroomName, callback) {
+
+		query = "SELECT m.message, u.username FROM messages m JOIN users u ON m.userid = u.userid WHERE m.chatroomid = (SELECT c.chatroomid FROM chatrooms c WHERE c.name = '" + chatroomName + "')"
+
+		client.query(query, (err, res) => {
+
+			if (err) {
+				console.log(err.stack)
+				return 
+			} else {
+				return callback(res)
+			}
+		})
+	},
+
+	// Saves message to database
+	saveMessage : function(msg, username, chatroomName) {
+
+		query = "INSERT INTO messages (message, chatroomid, userid) VALUES ('" + msg + "', (SELECT c.chatroomid FROM chatrooms c WHERE c.name = '" + chatroomName + "'), (SELECT u.userid FROM users u WHERE u.username = '" + username + "'))"
+
+		// Save message in DB
+		client.query(query, (err, res) => {
+
+			if (err) {
+	    		console.log(err.stack)
+	  		}
+		})
+	},
+
+	// Returns last chatroom a user was in if it is a valid login
+	handleLogin : function(username, password, callback) {
+
+		var query = "SELECT c.name FROM chatrooms c WHERE c.chatroomid = (SELECT u.lastchatroomid FROM users u WHERE u.username = '" + username + "' AND u.password = '" + password + "')"
+
+		// Save message in DB
+		client.query(query, function(err, res) {
+			if (err) {
+	    		console.log(err.stack)
+	    		return
+	  		}
+
+			return callback(res)
+		})
+	}
+}
--- src/index.js
+++ src/index.js
@@ -1,59 +1,105 @@
+
+// Imports
 var express = require('express')
 var app = express()
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
-var pg = require('pg')
 var path = require('path')
+var DB = require('./DB')
 
-var connectionString = 'postgres://localhost/ChatDB'
-const client = new pg.Client(connectionString);
-client.connect();
 
-function getHistory(socket) {
+var chatRoomsToUsers = {}
 
-	query = 'SELECT name FROM testtable'
+// Initialize mapping of valid rooms to its active users
+DB.init(function(res){
 
-	client.query(query, (err, res) => {
-		if (err) {
-			console.log(err.stack)
-		} else {
-			for (var i = 0; i < res.rows.length; i++){
-				socket.emit('chat message', res.rows[i]['name'])
-			}
-		}
+	// Populating dictionary
+	for (var i = 0; i < res.rows.length; i++){
+
+		chatRoomsToUsers[res.rows[i]['name']] = []
+	}
 })
-}
-
-function saveMessage(msg) {
-
-	query = 'INSERT INTO testtable (name) VALUES (\'' + msg + '\')'
-
-	// Save message in DB
-	client.query(query, (err, res) => {
-		if (err) {
-    		console.log(err.stack)
-  		}
-	})
-}
 
 // On a connection to a socket
 io.on('connection', function(socket){
 
-	// Populates screen with user's chat history
-	getHistory(socket)
+	var socketUsername
+	var usersChatrooms = []
+	var currentChatroom
+
+	// Joining a chatroom
+	socket.on('join room', function(roomName){
+
+		console.log("Room (" + roomName + ") joined by " + socketUsername)
+
+		// Updating list of users in chatrooms
+		chatRoomsToUsers[roomName].push(socketUsername)
+
+		// Updating the current chat room, setting socket to the room
+		currentChatroom = roomName
+		socket.join(currentChatroom)
+
+		// Populates screen with user's chat history
+		DB.getHistory(socketUsername, currentChatroom, function(res){
+			for (var i = 0; i < res.rows.length; i++){
+				socket.emit('chat message', res.rows[i]['username'] + "- " + res.rows[i]['message'])
+			}
+		})
+
+		io.to(currentChatroom).emit('user state update', chatRoomsToUsers[currentChatroom])
+
+		console.log("Chatroom layout- ")
+		console.log(chatRoomsToUsers)
+	})
+
+	socket.on('disconnect', function(){
+
+		console.log(socketUsername + " has left the " + currentChatroom + " room")
+		chatRoomsToUsers[currentChatroom] = chatRoomsToUsers[currentChatroom].filter(function(value, index, arr){
+
+		    return value != socketUsername;
+		});
+
+		io.to(currentChatroom).emit('user state update', chatRoomsToUsers[currentChatroom])
+	})
 
 	// On receiving a chat message
-	socket.on('chat message', function(msg){
+	socket.on('chat message', function(info){
 
-		saveMessage(msg)
+		// Save message in DB
+		DB.saveMessage(info['message'], socketUsername, currentChatroom)
+
+		console.log("Sending message- " + info['message'] + " to chatroom- " + currentChatroom)
 
 		// Sends message back to html to display it
-		io.emit('chat message', msg);
-	});
+		io.to(currentChatroom).emit('chat message', info['username'] + "- " + info['message']);
+	})
+
+	// Initializing the user's info on login
+	socket.on('user connect', function(info){
+		
+		// Querying DB if login is valid
+		DB.handleLogin(info['username'], info['password'], function(res){
+
+			// Invalid login
+			if (res.rows === undefined || res.rows.length == 0) {
+
+				console.log("Invalid login")
+			}
+
+			// Valid login
+			else{
+
+				// Sets the name of the socket
+				socketUsername = info['username']
+
+				// Transitions user to chat page, passes back the chatroom name to join
+				socket.emit('valid login', res.rows[0]['name'])
+			}
+		})
+	})
 });
-
-
 
 // HTTP server listening
 http.listen(port, function(){
